dcf5b8dc06a88eb1aed5b75505e887ec644048a9
csr13 <carsaroca.13@gmail.com>
2021-04-28 19:55:55 -0500

    {+} post vim plugin install stuff

--- post-vim.sh
+++ post-vim.sh
@@ -3,3 +3,5 @@
 wget -O nginx.vim http://www.vim.org/scripts/download_script.php\?src_id\=19394
 mkdir -p ~/.vim/syntax
 mv nginx.vim ~/.vim/syntax
+touch ~/.vim/filetype.vim
+echo "au BufRead,BufNewFile /etc/nginx/*,/usr/local/nginx/conf/* if &ft == '' | setfiletype nginx | endif" > filetype.vim
