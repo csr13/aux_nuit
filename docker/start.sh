002900711222c50b182c3ced026f8b85c6cf0d9a
csr13 <carsaroca.13@gmail.com>
2021-07-12 12:23:52 -0500

    {~} update

--- docker/start.sh
+++ docker/start.sh
@@ -32,14 +32,15 @@ sudo apt-get -y remove docker \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "$DOCKER_SRC $LSB_RELEASE stable" | sudo tee /etc/apt/sources.list.d/docker.list> /dev/null
+ echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # =======
 # Install
 # =======
 
-sudo apt-get -y update \
-    && \
+sudo apt-get -y update 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
