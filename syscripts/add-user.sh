de8f29eaae48f704dff19e85892f1124fabc4b43
csr13 <carsaroca.13@gmail.com>
2021-07-20 11:50:11 -0500

    {~} fixed4u

--- syscripts/add-user.sh
+++ syscripts/add-user.sh
@@ -11,23 +11,30 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
-if [ "$#" -ne 1 ]; then
-    echo "[!] usage ~> add-user.sh <username>"
+if [ "$#" -ne 2 ]; then
+    echo "[!] usage ~> add-user.sh <username> <sisudo|nosudo>"
     exit 1
 fi
 
 
 user=$1
 user_dir=/home/$user
+to_sudo=$2
 
 
 (
-    groupadd $user && \
-    useradd -s /bin/bash -d $user_dir $user && \
+    groupadd --force $user && \
+    useradd -s /bin/bash -d $user_dir -g $user,users $user && \
     mkdir -p $user_dir && \
     chown $user:$user -R $user_dir
 )
 
+
+if [ $to_sudo = "sisudo" ];
+    usermod -G sudo,$user,users,www-data $user
+else
+    usermod -G $user,users,www-data $user
+fi
 
 echo "[!] set new password for $user -- make it funky"
 passwd $user
