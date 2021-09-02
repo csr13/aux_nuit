aae53c0b7d3757afbea71c4b69a8aaafb66cc87c
csr13 <carsaroca.13@gmail.com>
2021-09-01 23:54:34 -0500

    {+} system scripts

--- setup.sh
+++ setup.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
+# ~+~+~+~+~+~+~+~+~+~+~+~+~+
 
 for et in $(ls . | sort); do
-    if [ $et == "git" ]; then
+    if [ $et == "git" ] || [ $et != "syscripts" ]; then
         continue
     fi
+    # ~+~+~+~+~+~+~+
     # être certain
+    # ~+~+~+~+~+~+~+
     exe="$et/start.sh"
     if [ -f $exe ]; then
-        if [[ ! "dot" =~ $et ]]; then
+        if [[ ! "dot" =~ $et ]]; then 
+            # ~+~+~+~+~+~+~+~+~+
             # rendre exécutable
+            # ~+~+~+~+~+~+~+~+~+       
             chmod +x $exe;
-            # exécuter l'exécutable
             /bin/bash "$(pwd)/$exe"
         fi
-        if [[ "vim" =~ $et ]]; then
+        if [[ "vim" =~ $et ]]; then   
+            # ~+~+~+~+~+~+~+~
             # existe-t-il ??
+            # ~+~+~+~+~+~+~+~   
             vimrc="$(pwd)/$et/.vimrc"
             if [ -f $vimrc ]; then
+                # ~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~
                 # ça existe
                 # copier le fichier de configuration
+                # ~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~
                 cp -v $vimrc ~/
             fi
         fi
+    # ~+~+~+~+~+~+~+~+~+~+~+~+~+
     # choses cachées ..
-    elif [ $et == "dot" ]; then 
+    # ~+~+~+~+~+~+~+~+~+~+~+~+~+
+    elif [ $et == "dot" ]; then
+        # ~+~+~+~+~+
         # explicite
+        # ~+~+~+~+~+
         escape=$(ls -A "$(pwd)/$et"| egrep '^\.');
         for toi in $escape; do
             moi="$et/$toi";
@@ -33,7 +45,7 @@ for et in $(ls . | sort); do
                 cp -v $loin ~/;
             fi # je ne sais pa
         done
-    fi # je ne sais plus 
-done 
+    fi # je ne sais plus
+done
 # je suis perdu
-    
+
