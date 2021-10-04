787b412929ff4c0f3644b90b0208afe69456731c
csr13 <carsaroca.13@gmail.com>
2021-10-03 21:44:45 -0500

    {+} update + docker-compose

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 sudo apt-get -y remove docker \
     docker-engine \
     docker.io \
@@ -14,9 +15,10 @@ sudo apt-get -y remove docker \
         gnupg \
         lsb-release
 
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
- echo \
+echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
@@ -24,4 +26,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 sudo apt-get -y update 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
+if [ ! which docker-compose ]; then
+     sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+fi
