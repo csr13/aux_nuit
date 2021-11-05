74096a69c239627fd5c486c5b6dbf9c90816b7d9
csr13 <carsaroca.13@gmail.com>
2021-11-05 00:24:42 -0600

    {+} updatez

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM ubuntu:focal
 ARG TAGS
-WORKDIR /home/root
+WORKDIR /home/root/tools
 ARG DEBIAN_FRONTEND=noninteractive
 RUN apt update && \
     apt install -y software-properties-common && \
     apt-add-repository -y ppa:ansible/ansible && \
     apt update && \
-    apt install -y curl git ansible build-essential
+    apt install -y curl wget git ansible build-essential
 COPY . .
 CMD ["sh", "-c", "ansible-playbook $TAGS local.yml"]
--- vim/start.sh
+++ vim/start.sh
@@ -2,27 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get -y install git 
-apt-get -y install make 
-apt-get -y install clang
-apt-get -y install libxt-dev
-apt-get -y install libpython3.9-dev
-
-git clone https://github.com/vim/vim.git /tmp/vim 
-
-cd /tmp/vim
-make
-make test
-make install
-
-vim --version
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-chmod +x post-vim.sh && post-vim.sh
+chmod +x vim/post-vim.sh && vim/post-vim.sh
 
-cp .vimrc ~/.vimrc
-
-echo "[!] run vim -c 'PluginInstall'"
-
+cp vim/.vimrc ~/.vimrc
