89de03ec338da268ee2dfd1d821a6c2aa3c83ac2
csr13 <37247816+csr13@users.noreply.github.com>
2022-05-01 10:56:42 -0500

    {~} >.<

--- services/zap/setup-zap-service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f ./zapdaemon.service ]; then
-    sudo cp ./zapdaemon.service /etc/systemd/system/zapdaemon.service;
-    sudo systemctl daemon-reload
-    sudo systemctl start zapdaemon.service
-else
-    echo "zapdaemon.service not found"
-fi
