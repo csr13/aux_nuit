49f5948439de9a9deaa3efed0c3c05c298bb81a3
csr13 <carsaroca.13@gmail.com>
2022-05-01 02:51:49 +0000

    {+} update

--- services/dfirtrack/build.sh
+++ services/dfirtrack/build.sh
@@ -1,18 +1,18 @@
-
 #!/bin/bash
 
+apt-get install -y python3.8 \
+    python3.8-venv \
+    python3.8-dev \
+    python3-dev \
+    libpq-dev
 
 git clone https://github.com/stuhli/dfirtrack.git ~/src
-python3.8 -m venv  ~/src/venv
+python3.8 -m venv ~/src/venv
+
 source ~/src/venv/bin/activate
-pip install -r ~/src/requirements.txt
-cd ~/src
+pip install wheel django psycopg2-binary
+cd ~/src && pip install -r requirements.txt && python manage.py makemigrations && \
+    python manage.py migrate && \
+    python manage.py createcachetable && \
+    python manage.py createsuperuser
 
-# Migrate and run commands.
-python3 manage.py migrate
-python3 manage.py createcachetable
-python3 manage.py createsuperuser
-
-if [ $1 = keepalive ]; then
-    python3 manage.py runserver 
-fi;
