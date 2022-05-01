689425a5744a6d754c13bbbea6dbaa3539d9d07b
csr13 <37247816+csr13@users.noreply.github.com>
2022-04-30 19:35:36 -0500

    {+} oppssies

--- django/start.sh
+++ django/start.sh
@@ -1,17 +1,1 @@
 #!/bin/bash
-
-
-git clone https://github.com/stuhli/dfirtrack.git ~/src
-python3.8 -m venv  ~/src/venv
-source ~/src/venv/bin/activate
-pip install -r ~/src/requirements.txt
-cd ~/src
-
-# Migrate and run commands.
-python3 manage.py migrate
-python3 manage.py createcachetable
-python3 manage.py createsuperuser
-
-if [ $1 = keepalive ]; then
-    python3 manage.py runserver 
-fi;
