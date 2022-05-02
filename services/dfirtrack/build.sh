bd968886f63531658f2d1547ca4a0f6c35b319b0
csr13 <carsaroca.13@gmail.com>
2022-05-01 22:38:18 -0500

    >.< me parecio ver una linda gatita >,<

--- services/dfirtrack/build.sh
+++ services/dfirtrack/build.sh
@@ -9,10 +9,20 @@ apt-get install -y python3.8 \
 git clone https://github.com/stuhli/dfirtrack.git ~/src
 python3.8 -m venv ~/src/venv
 
+cat << EOF > $HOME/src/dfirtrack/local_settings.py
+from dfirtrack.settings import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1']
+EOF
+
 source ~/src/venv/bin/activate
 pip install wheel django psycopg2-binary
-cd ~/src && pip install -r requirements.txt && python manage.py makemigrations && \
-    python manage.py migrate && \
-    python manage.py createcachetable && \
-    python manage.py createsuperuser
+cd ~/src && pip install -r requirements.txt && \
+    python manage.py makemigrations --settings=dfirtrack.local_settings.py && \
+    python manage.py migrate --settings=dfirtrack.local_settings.py && \
+    python manage.py createcachetable --settings=dfirtrack.local_settings.py && \
+    python manage.py createsuperuser --settings=dfirtrack.local_settings.py
 
+echo "[SERVICE MANAGER] run: python manage.py runserver to start
