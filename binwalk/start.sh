04216d379b88ed03faefb07686c4c2c5a06eb497
csr13 <carsaroca.13@gmail.com>
2022-06-30 18:27:13 -0500

    {+} update binwalk

--- binwalk/start.sh
+++ binwalk/start.sh
@@ -2,5 +2,9 @@
 
 git clone https://github.com/ReFirmLabs/binwalk.git ~/binwalk
 
+if [ ! $(which python3) ] || [ ! $(which python3.8) ]; then
+    cd .. && /bin/bash setup.sh python
+fi;
+
 cd ~/binwalk && python3 setup.py install
 
