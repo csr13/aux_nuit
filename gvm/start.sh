be351f198627ed389c81c5479efbd1519180be26
csr13 <carsaroca.13@gmail.com>
2022-09-29 05:25:07 +0000

    {+} update

---
+++ gvm/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+###########################################################
+# Build script for greenbone 21.4 on Ubuntu Maniches
+# Postgresql 13 and nothing else for storage, mostly for standalone servers with
+# http access on a vpn or something similar, maybe some api wrapper around openvas
+# with python-gvmd. and flask.
+###########################################################
+
+# TO DO
+
