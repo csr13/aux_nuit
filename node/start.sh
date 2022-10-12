4b9f273ce8c104072ed5df7273a460335401fbc0
csr13 <37247816+csr13@users.noreply.github.com>
2022-10-12 14:39:30 -0600

    Update start.sh

--- node/start.sh
+++ node/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Using Ubuntu
-curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash - &&\
 sudo apt-get -y update && sudo apt-get install -y nodejs
+
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 
