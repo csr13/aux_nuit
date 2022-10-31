d69ff737fcb275908c6499a9215ac97b5faa1973
csr13 <37247816+csr13@users.noreply.github.com>
2022-10-31 10:03:43 -0700

    Update start.sh

--- crons/start.sh
+++ crons/start.sh
@@ -2,8 +2,4 @@
 
 # update sources hourly.
 mv ./update-sources.sh /etc/cron.hourly/update-sources && \
-  chmod +x /etc/cron.hourly/update-sources.sh
-
-# to do: DNS log analysis and ssh hourly for sending alerts.
-# not very reasonable perhaps every 15 seconds works best for real time
-# but beats the purpose of demonstration.
+  chmod +x /etc/cron.hourly/update-sources
