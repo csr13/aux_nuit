02ef12e2160212f5e9b3746832005fdb024540be
csr13 <37247816+csr13@users.noreply.github.com>
2023-02-28 21:49:28 -0600

    Update start.sh

--- vim/start.sh
+++ vim/start.sh
@@ -2,8 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 chmod +x vim/post-vim.sh && vim/post-vim.sh
 
 cp vim/.vimrc ~/.vimrc
